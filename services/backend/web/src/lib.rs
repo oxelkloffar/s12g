d040f4f7ee1761a3778a4a9b0c3736196879dcec
RichoDemus <richodemus@gmail.com>
2019-08-25 20:44:27 +0200

    Add an example endpoint to the api

--- services/backend/web/src/lib.rs
+++ services/backend/web/src/lib.rs
@@ -1,8 +1,13 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 #[macro_use]
 extern crate rocket;
+#[macro_use]
+extern crate rocket_contrib;
+#[macro_use]
+extern crate serde_derive;
 
 use rocket::Rocket;
+
 mod routes;
 
 pub fn rocket() {
--- services/backend/web/src/routes.rs
+++ services/backend/web/src/routes.rs
@@ -1,11 +1,38 @@
 use rocket::Route;
+use rocket_contrib::json::Json;
+use rocket_contrib::uuid::Uuid;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[derive(Deserialize)]
+struct CreateNodeRequest {
+    name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+struct Node {
+    id: String,
+    name: String,
+}
+
+/*
+curl -X PUT \
+-H "Content-Type: application/json" \
+-d "{\"name\":\"richo\"}" \
+localhost:8000/api/v1/nodes/00000000-0000-0000-0000-000000000000
+*/
+#[put("/api/v1/nodes/<id>", data = "<create_node_request>", format = "json")]
+fn add_node(id: Uuid, create_node_request: Json<CreateNodeRequest>) -> Json<Node> {
+    println!("Add node: {} - {}", id, create_node_request.name);
+    Json(Node {
+        id: id.to_string(),
+        name: create_node_request.name.clone(),
+    })
+}
+
 pub fn routes() -> Vec<Route> {
-    let asd:Vec<Route> = routes![index];
-    return asd
+    routes![index, add_node]
 }
